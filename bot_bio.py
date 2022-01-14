7af8e24bdeebe670a81ece2a5acdb8c0858bdae1
Rakshit <70335252+spuckhafte@users.noreply.github.com>
2022-01-14 22:33:08 +0530

    Update bot_bio.py

--- bot_bio.py
+++ bot_bio.py
@@ -1,8 +1,13 @@
 class Behaviour:
     bot_token = ''
-    bot_prefix = 'cry-'
-    cry_factor = None  # number
-    hash_sign = ''
-    default_hash = '0000000000000000000000000000000000000000000000000000000000000000'
-    time_out = 0
-    default_amount = 0
+    bot_prefix = ''
+    cry_factor = None  # number, showing the most average amount amount of cries (eg. 0.0000001)
+    hash_sign = '' # text that should be in the start of every hash of a transaction (eg. 2005cc34trg35yt...)
+    default_hash = '0000000000000000000000000000000000000000000000000000000000000000' # default hash, for the first block (first block can't have a prev hash)
+    time_out = 0 # people can use 'cry-mine' in this time interval (minutes)
+    default_amount = 0 # 'cries' sent to the first lucky user (genesis block)
+    new_string_channel_id = 0 # id of a channel where all new "minable" strings could be sent
+    general_channel_category_id = 0 # id of "General" Category in the official server (people chill in this category)
+    test_category_id = 0 # id of a category in server named "Bot Testing"
+    help_chnl_id = 0 # id of a channel where users can seek help from the moderators
+    log_chnl_id = 0 # == new_string_channel_id (both the id are of the same channel, did it by mistake lol)
